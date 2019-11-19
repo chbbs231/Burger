1ff48f2e9bede6323ac783500aec7f65396b9ff7
chbbs231 <atrobles231@gmail.com>
2019-11-18 21:37:19 -0800

    routes

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const { join } = require ('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs'}))
+app.set('view engie', '.hbs')
+
+require('./routes')(app)
+
+
+app.listen(3002)
